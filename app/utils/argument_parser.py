32a436d1427d69adfa3a45e08377d3bcb95dae1c
sazzad <abdu.hasib@Infolytx.com>
2021-10-23 11:38:18 +0600

    implemented argument_parser

---
+++ app/utils/argument_parser.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+
+
+def parse_args_and_load_vars():
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="input_file",
+                        help="FILE to read input data from")
+
+    args = parser.parse_args()
+    if not args.input_file:
+        args.input_file = "../invalid_input.txt"
+
+    return args
