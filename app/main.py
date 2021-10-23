1f3cf0972197ac11d2ce2dd6e6ba56c4906ae067
sazzad <abdu.hasib@Infolytx.com>
2021-10-23 21:45:42 +0600

    refactored main file code

--- app/main.py
+++ app/main.py
@@ -1,32 +1,35 @@
+from typing import Optional
 from order import Order
 from order_info_transfomer import transform_order_info
 from recipe_maker import prepare_curry
 from input_validity_checker import check_input_validity
 from utils.argument_parser import parse_args_and_load_vars
 from utils.reader.text_file_reader import TextFileReader
-from curry_names import NO_SOLUTION_MSG
+from constants import NO_SOLUTION_MSG
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def main(lines: list):
+def main(lines: list) -> Optional[list]:
     is_invalid_input, total_recipe, total_customer = check_input_validity(lines)
-    if is_invalid_input:
-        logging.info(NO_SOLUTION_MSG)
+    curries = None
 
-    else:
+    if not is_invalid_input:
         order: Order = transform_order_info(lines, total_recipe, total_customer)
         curries = prepare_curry(order)
-        if not curries:
-            logging.info(NO_SOLUTION_MSG)
-        else:
-            logging.info("Final Curry list is :-")
-            logging.info(" ".join(curries))
+
+    return curries
 
 
 if __name__ == '__main__':
     args = parse_args_and_load_vars()
     line_arr: list = TextFileReader().read(args.input_file)
-    main(line_arr)
+    result = main(line_arr)
+
+    if not result:
+        logging.info(NO_SOLUTION_MSG)
+    else:
+        logging.info("Final Curry list is :-")
+        logging.info(" ".join(result))
 
