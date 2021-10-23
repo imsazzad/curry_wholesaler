e30d30c3493f85e5495e6bc11f5a9603934c4759
sazzad <abdu.hasib@Infolytx.com>
2021-10-23 22:31:33 +0600

    moved customer_info and curry_info class to a different module

---
+++ app/curry_info.py
@@ -0,0 +1,4 @@
+class CurryInfo:
+    def __init__(self, curry_number: int = -1, curry_name: str = None):
+        self.curry_number = curry_number
+        self.curry_name = curry_name
---
+++ app/customer_info.py
@@ -0,0 +1,4 @@
+class CustomerInfo:
+    def __init__(self, idx=None, num_of_preference=float("inf")):
+        self.idx = idx
+        self.num_of_preference = num_of_preference
