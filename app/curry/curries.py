f30bfc0bc4798363606afea60ac17d88304b1fdb
sazzad <abdu.hasib@Infolytx.com>
2021-10-24 17:14:09 +0600

    refactored curry related code into curry package

---
+++ app/curry/curries.py
@@ -0,0 +1,14 @@
+from app.constants import VEGETARIAN, MEAT
+from app.curry.curry_info import CurryInfo
+
+
+def get_next_curry_number(current_customer_info) -> CurryInfo:
+    next_curry_info = CurryInfo()
+    for current_curry_number in current_customer_info:
+        current_curry_name = current_customer_info[current_curry_number]
+        can_replace_next_curry = not next_curry_info.curry_name or (current_curry_name == VEGETARIAN
+                                                                    and next_curry_info.curry_name == MEAT)
+        if can_replace_next_curry:
+            next_curry_info = CurryInfo(curry_number=current_curry_number, curry_name=current_curry_name)
+
+    return next_curry_info
